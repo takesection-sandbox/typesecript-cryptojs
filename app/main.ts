2f4e32c57a692542943a1ad745e947d4d7a3a850
Shigeki Shoji <shige@pigumer.gr.jp>
2016-09-22 17:17:36 +0900

    Update typed function

--- app/main.ts
+++ app/main.ts
@@ -1,8 +1,12 @@
 let cryptoJS: CryptoJS.CryptoJSStatic = require("crypto-js");
 
+function signature(message: string, key: string): string {
+    return cryptoJS.HmacSHA256(message, key).toString(cryptoJS.enc.Base64);
+}
+
 let key: string = "1234567890";
 let message: string = "message";
 
-let sig: string = cryptoJS.HmacSHA256(message, key).toString(cryptoJS.enc.Base64);
+let sig: string = signature(message, key);
 
 console.log(sig);
