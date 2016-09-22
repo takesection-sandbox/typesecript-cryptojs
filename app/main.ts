5e16188a00bcae3d0d46508750a49454cbfae7c7
Shigeki Shoji <shige@pigumer.gr.jp>
2016-09-22 17:20:20 +0900

    Rename function

--- app/main.ts
+++ app/main.ts
@@ -1,12 +1,12 @@
 let cryptoJS: CryptoJS.CryptoJSStatic = require("crypto-js");
 
-function signature(message: string, key: string): string {
+function sign(message: string, key: string): string {
     return cryptoJS.HmacSHA256(message, key).toString(cryptoJS.enc.Base64);
 }
 
 let key: string = "1234567890";
 let message: string = "message";
 
-let sig: string = signature(message, key);
+let sig: string = sign(message, key);
 
 console.log(sig);
