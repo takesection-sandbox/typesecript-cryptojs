9e6fcdf823c387ba2ca0dcf3ad00fd1088faa81a
Shigeki Shoji <shige@pigumer.gr.jp>
2016-09-25 14:35:57 +0900

    Fix tsconfig.json

--- app/main.ts
+++ app/main.ts
@@ -1,12 +1,12 @@
-let cryptoJS: CryptoJS.CryptoJSStatic = require("crypto-js");
+var CryptoJS: CryptoJS.CryptoJSStatic = require('crypto-js');
 
 function sign(message: string, key: string): string {
-    return cryptoJS.HmacSHA256(message, key).toString(cryptoJS.enc.Base64);
+    return CryptoJS.HmacSHA256(message, key).toString(CryptoJS.enc.Base64);
 }
 
-let key: string = "1234567890";
-let message: string = "message";
+const key: string = "1234567890";
+const message: string = "message";
 
-let sig: string = sign(message, key);
+const sig: string = sign(message, key);
 
 console.log(sig);
